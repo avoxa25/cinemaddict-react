389f1a724bfe946c30f005ab0b39b91f8a9c8a12
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-13 19:25:29 +0300

    Add app component

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export function App(): JSX.Element {
+    return (
+        <h1>There will be placed markup of Cinemaddict</h1>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import * as ReactDOM from 'react-dom';
 
 import './global-styles.scss';
 
+import { App } from './components/app/app';
+
 ReactDOM.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
