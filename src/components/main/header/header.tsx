b35bcd652eb3a61432bdceb2c5ebfa96bfd2c982
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-19 20:02:23 +0300

    Add header component

---
+++ src/components/main/header/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header(): JSX.Element {
+    return (
+        <nav className="main-navigation">
+            <div className="main-navigation__items">
+                <a href="#all" className="main-navigation__item main-navigation__item--active">All movies</a>
+                <a href="#watchlist" className="main-navigation__item">Watchlist <span className="main-navigation__item-count">13</span></a>
+                <a href="#history" className="main-navigation__item">History <span className="main-navigation__item-count">4</span></a>
+                <a href="#favorites" className="main-navigation__item">Favorites <span className="main-navigation__item-count">8</span></a>
+            </div>
+            <a href="#stats" className="main-navigation__additional">Stats</a>
+        </nav>
+    )
+}
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
+import Header from './header/header';
 
 export function Main(): JSX.Element {
     return (
         <main className="main">
-            <nav className="main-navigation">
-                <div className="main-navigation__items">
-                    <a href="#all" className="main-navigation__item main-navigation__item--active">All movies</a>
-                    <a href="#watchlist" className="main-navigation__item">Watchlist <span className="main-navigation__item-count">13</span></a>
-                    <a href="#history" className="main-navigation__item">History <span className="main-navigation__item-count">4</span></a>
-                    <a href="#favorites" className="main-navigation__item">Favorites <span className="main-navigation__item-count">8</span></a>
-                </div>
-                <a href="#stats" className="main-navigation__additional">Stats</a>
-            </nav>
+            <Header />
 
             <ul className="sort">
                 <li><a href="#" className="sort__button sort__button--active">Sort by default</a></li>
